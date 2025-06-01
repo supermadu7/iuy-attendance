1693fe915a3e31596258be80a3a64fe6f053dc3e
Ifeanyi Madu <supermadu7@gmail.com>
2025-06-01 01:26:12 +0100

    update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
  <footer class="footer mt-auto py-3 bg-white text-center">
      <div class="container">
          <span class="text-muted"> Copyright © <span id="year"></span> <a href="javascript:void(0);"
-                 class="text-dark fw-semibold">Spruha</a>.
+                 class="text-dark fw-semibold">IUY</a>.
              Designed with <span class="bi bi-heart-fill text-danger"></span> by <a href="javascript:void(0);">
                  <span class="fw-semibold text-primary text-decoration-underline">Spruko</span>
              </a> All
