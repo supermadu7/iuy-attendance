fb2446f112e8d33bf862c91e76d56ac763765f54
Ifeanyi Madu <supermadu7@gmail.com>
2025-06-02 06:16:49 +0100

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,7 +9,7 @@
             <h2 class="main-content-title fs-24 mb-1">Welcome To Dashboard</h2>
             <ol class="breadcrumb mb-0">
                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Project Dashboard</li>
+                <li class="breadcrumb-item active" aria-current="page"> Dashboard</li>
             </ol>
         </div>
         <div class="d-flex">
