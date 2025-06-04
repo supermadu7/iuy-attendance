6814d31bc77ef8e969b43977f7e75944c8a68cc4
Ifeanyi Madu <supermadu7@gmail.com>
2025-06-04 08:15:01 +0100

    update dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
     <!-- Start::page-header -->
     <div class="d-md-flex d-block align-items-center justify-content-between page-header-breadcrumb">
         <div>
-            <h2 class="main-content-title fs-24 mb-1">Welcome To Dashboard</h2>
+            <h2 class="main-content-title fs-24 mb-1">Welcome To Your Dashboard</h2>
             <ol class="breadcrumb mb-0">
                 <li class="breadcrumb-item"><a href="#">Home</a></li>
                 <li class="breadcrumb-item active" aria-current="page"> Dashboard</li>
